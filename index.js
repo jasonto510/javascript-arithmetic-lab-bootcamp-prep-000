0e05fab6bae60383532ed829b0fb14ea3031dd28
Jason To <jasto@ucdavis.edu>
2018-09-12 07:09:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ function inc(n){
   return (n + 1);
 }
 
+function dec(n){
+  return (n - 1 );
+}
+
+function makeInt(n){
+  return parseInt(n);
+}
 divideBy3()
  
 console.log(number) // 3.333333333335
